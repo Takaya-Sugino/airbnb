d720249f952aef2daeb4a1f5df1c6aec5efb215f
Takaya-Sugino <takaya.sugino@kellogg.northwestern.edu>
2022-01-15 03:11:00 +0000

    practice1-7_done

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,7 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-
+SELECT listings.neighborhood, count(reviews.id)
+FROM listings
+Inner Join reviews on listings.id = reviews.listing_id
+GROUP BY listings.neighborhood
